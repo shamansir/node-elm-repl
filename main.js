7599a72f5ae73f7918931e6c2a79b937bcbb0a6c
Mukesh Soni <mukeshsoni@Mukeshs-MacBook-Pro-2.local>
2016-10-06 12:37:11 +0530

    tweaks to get correct result from elm-repl

--- main.js
+++ main.js
@@ -5,56 +5,70 @@ const
     async = require( 'async' ),
     repl = spawn( 'elm-repl' );
 
-const lines = [
-    'add b c = b + c',
-    'import String',
-    'other = String.join "a" ["H","w","ii","n"]'
-];
+setTimeout(function() {
+  const lines = [
+      'add b c = b + c',
+      '12+12',
+      'import String',
+      'String.join "a" ["H","w","ii","n"]',
+      '12+322',
+      '12+323',
+      '1-4'
+  ];
 
-let stdouts = [];
+  let stdouts = [];
 
-let replStarted = false;
+  let replStarted = false;
 
-let waitsForResponse = null;
+  let waitsForResponse = null;
 
-repl.stdout.on( 'data', data => {
-    console.log( `stdout: [[[ ${data} ]]] ` );
-    if (!replStarted) {
-        replStarted = true;
-        async.nextTick(writeLines);
-    } else if (waitsForResponse) {
-        waitsForResponse(data);
-        waitsForResponse = null;
-    }
-});
+  repl.stdout.on( 'data', data => {
+      console.log( `from repl: \n [[[ ${data} ]]] \n\n` );
+      if (!replStarted) {
+          replStarted = true;
+          async.nextTick(writeLines);
+      } else if (waitsForResponse) {
+          waitsForResponse(data);
+          waitsForResponse = null;
+      }
+  });
 
-repl.stderr.on( 'data', data => {
-    console.log( `stderr: ${data} ` );
-});
+  repl.stderr.on( 'data', data => {
+      console.log( `stderr: ${data} ` );
+  });
 
-repl.on( 'close', code => {
-    console.log( `child process exited with code [[[ ${code} ]]]` );
-});
+  repl.on( 'close', code => {
+      console.log( `child process exited with code [[[ ${code} ]]]\n\n` );
+  });
 
-var q = async.queue(function(data, callback) {
-    console.log( `sending line [[[ ${data.line} ]]]`);
-    repl.stdin.write(data.line + '\n', null, function() {
-        waitsForResponse = function(response) {
-            console.log( `${data.line} was executed, last stdout is [[[ ${response} ]]]`);
-            callback(response);
-        };
-    });
-});
+  var q = async.queue(function(data, callback) {
+      console.log( `sending line [[[ ${data.line} ]]]\n\n`);
+      repl.stdin.write(data.line + '\n', function(err) {
+        // console.log(`inside stdin.write callback. wrote ${data.line}`)
+        if(err) {
+          console.log('error writing to stdid', err.toString())
+        }
+          waitsForResponse = function(response) {
+              console.log( `${data.line} === ${response}\n\n`);
+              setTimeout(function() {
+                callback(response);
+              }, 1000)
+          };
+      });
+  });
 
-q.drain = function() {
-    console.log('all lines were sent, killing repl');
-    repl.kill();
-};
+  q.drain = function() {
+      console.log('all lines were sent, killing repl');
+      setTimeout(() => {
+        repl.kill();
+      }, 5000)
+  };
 
 
-function writeLines() {
-    console.log('write lines called');
-    lines.forEach(function(line) {
-        q.push({ line: line });
-    });
-}
+  function writeLines() {
+      console.log('write lines called');
+      lines.forEach(function(line) {
+          q.push({ line: line });
+      });
+  }
+}, 500)
