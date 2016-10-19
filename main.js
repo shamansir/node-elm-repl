fd06e8ada610ed48a8061c7047de849a4e9b43b9
Ulric Wilfred <shaman.sir@gmail.com>
2016-10-19 11:31:04 +0200

    be able to parse exports, move completed successfully

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 // version
 
 var versionParser = new Parser()
-                    .skip(4).int32('major')
-                    .skip(4).int32('minor')
-                    .skip(4).int32('patch');
+    .skip(4).int32('major')
+    .skip(4).int32('minor')
+    .skip(4).int32('patch');
 
 // package info
 
@@ -45,13 +45,33 @@ var importsFormatter = function(v) {
 
 // exports
 
-var exportPathItemParser = new Parser();
+var exportPathItemParser = new Parser()
+    .skip(4).int32('itemLen')
+    .string('item', { length: 'itemLen' });
 
-var exportPathParser = new Parser();
+var singleExportParser = new Parser()
+    .skip(4).int32('count')
+    .array('path', {
+        type: exportPathItemParser,
+        length: 'count'
+    });
 
-var singleExportParser = new Parser();
+var exportsParser = new Parser()
+    .skip(4).int32('count')
+    .array('values', {
+        type: singleExportParser,
+        length: 'count'
+    });
 
-var exportsParser = new Parser();
+var exportsFormatter = function(v) {
+    return v.values.map(function(ev) {
+        return {
+            path: ev.path.map(function(pv) {
+                      return pv.item;
+                  })
+        }
+    });
+};
 
 // types
 
@@ -65,7 +85,8 @@ var elmiParser = new Parser()
                            formatter: packageInfoFormatter })
         .nest('imports', { type: importsParser,
                            formatter: importsFormatter })
-        .nest('exports', { type: exportsParser })
+        .nest('exports', { type: exportsParser,
+                           formatter: exportsFormatter })
         .nest('types',   { type: typesParser });
 
 fs.readFile('./Anagram.elmi', function(_, stream) {
@@ -73,126 +94,6 @@ fs.readFile('./Anagram.elmi', function(_, stream) {
     console.dir(interface);
     logInterface(interface);
 });
-
-//logInterface(elmiParser.parse(fs.readFile('./Anagram.elmi')));
-
-/* fs.readFile('./Anagram.elmi', function(_, stream) {
-    vars = binary.parse(stream)
-        .into('version', parseCompilerVersion)
-        .into('package', parsePackageName)
-        .into('imports', parseImports)
-        .into('exports', parseExports)
-        .into('types', parseTypes)
-        .vars
-
-    console.dir(vars);
-    logInterface(vars);
-});
-
-
-function parseCompilerVersion() {
-    this.word64bs('major')
-        .word64bs('minor')
-        .word64bs('patch');
-}
-
-function parsePackageName() {
-    this.tap(parseBStringAs('user'))
-        .tap(parseBStringAs('name'));
-}
-
-function parseImports() {
-    this.word64bs('count')
-        .tap(function(vars) {
-            vars.pos = vars.count;
-            vars.values = [];
-        })
-        .loop(function(end, vars) {
-            if (vars.pos <= 0) {
-                delete vars.pos;
-                delete vars.lastType;
-                end();
-                return;
-            }
-            vars.pos--;
-            this.word8bs('lastType')
-                .tap(function(vars) {
-                    parseBString(this, function(v) {
-                        vars.values.push({
-                            'type': vars.lastType,
-                            'name': v
-                        })
-                    });
-                })
-
-        });
-}
-
-function parseExports() {
-    this.word64bs('count')
-        .tap(function(vars) {
-            vars.pos = vars.count;
-            vars.values = [];
-        })
-        .loop(function(end, vars) {
-            if (vars.pos <= 0) {
-                delete vars.pos;
-                end();
-                return;
-            }
-            vars.pos--;
-            this.word64bs('lastCount')
-                .tap(function(vars) {
-                    vars.innerPos = vars.lastCount;
-                    vars.collected = [];
-                    vars.values.push({
-                        'count': vars.lastCount,
-                        'name': []
-                    });
-                })
-                .loop(function(end, vars) {
-                    if (vars.innerPos <= 0) {
-                        delete vars.innerPos;
-                        delete vars.lastCount;
-                        delete vars.collected;
-                        end();
-                        return;
-                    }
-                    vars.innerPos--;
-                    parseBString(this, function(v) {
-                        vars.values[vars.values.length - 1].name.push(v);
-                    });
-                })
-
-        });
-}
-
-function parseTypes() {
-}
-
-function parseBString(buff, callback) {
-    var tempLenKey = '___tempLen';
-    var tempKey = '___temp';
-    buff.word64bs(tempLenKey)
-        .buffer(tempKey, tempLenKey)
-        .tap(function(vars) {
-            var value = vars[tempKey];
-            var len = vars[tempLenKey];
-            delete vars[tempLenKey];
-            delete vars[tempKey];
-            callback(value.toString(), len);
-        });
-}
-
-function parseBStringAs(label) {
-    return function() {
-        this.word64bs(label)
-            .buffer(label, label)
-            .tap(function(vars) {
-                vars[label] = vars[label].toString();
-            });
-    }
-} */
 
 function logInterface(iface) {
 
@@ -203,10 +104,10 @@ function logInterface(iface) {
     while (i--) {
         console.log('- ', '[' + i + ']', iface.imports[i].type, ' : ', iface.imports[i].name);
     }
-    // console.log('imports:', iface.exports.count);
-    // i = iface.exports.count;
-    // while (i--) {
-    //     console.log('- ', '[' + i + ']', iface.exports.values[i].count, ' : ', iface.exports.values[i].name.join('/'));
-    // }
+    console.log('exports:', iface.exports.length);
+    i = iface.exports.length;
+    while (i--) {
+        console.log('- ', '[' + i + ']', iface.exports[i].path.join('/'));
+    }
 
 }
