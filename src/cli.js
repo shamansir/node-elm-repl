120ade1c6a06de01d6bbdaa75406eccff83b22f8
Ulric Wilfred <shaman.sir@gmail.com>
2017-10-08 14:10:56 +0200

    move help text to a separate file

--- src/cli.js
+++ src/cli.js
@@ -1,47 +1,14 @@
 const Repl = require('./repl.js');
+const helpText = require('./cli-help.js');
 
-var argv = require('minimist')(process.argv.slice(2));
+const argv = require('minimist')(process.argv.slice(2));
 
 if (!argv.from) {
-    console.log('');
-    console.log('Node-Elm-REPL.');
-    console.log('');
-    console.log('Prepare a file and put all your elm expressions there, line by line');
-    console.log('If you need imports, list them in the first line starting with `;` and splitting them with `;`');
-    console.log('');
-    console.log('For example (the contents of `src/cli-example`):');
-    console.log('');
-    console.log(';List as L;Maybe exposing ( Maybe(..) );String');
-    console.log('L.map');
-    console.log('L.foldl');
-    console.log('Just');
-    console.log('Nothing');
-    console.log('1 + 1');
-    console.log('\\a b -> a + b');
-    console.log('');
-    console.log('Then run `node src/cli.js --from <your-file-name>`');
-    console.log('');
-    console.log('And you should get:');
-    console.log('');
-    console.log('(a -> b) -> List a -> List b');
-    console.log('(a -> b -> b) -> b -> List a -> b');
-    console.log('a -> Maybe.Maybe a');
-    console.log('Maybe.Maybe a');
-    console.log('number');
-    console.log('number -> number -> number');
-    console.log('');
-    console.log('Other options:');
-    console.log('--work-dir — specify working directory for execution, for example where the `.elm` files you use are located');
-    console.log('--elm-ver — exact elm-version you use (default: 0.18.0), required to be known if you import your own modules');
-    console.log('--user — your username specified in elm-package.json (default: user), required to be known if you import your own modules');
-    console.log('--package — your project specified in elm-package.json (default: project), required to be known if you import your own modules');
-    console.log('--package-ver — your project version specified in elm-package.json (default: 1.0.0), required to be known if you import your own modules');
-    console.log('--show-time — show how much time it took to convert everyting');
-    console.log('--with-values — include values into the output (takes more time to extract them)');
-    console.log('--only-values — report and extract only values, not the types (overrides --with-values)');
-    console.log('--values-below — has sense only when --with-values was used: instead of putting types and values in lines like TYPE<TAB>VALUE, put a list of values line-by-line below the list of types: could be useful for parsing');
-    console.log('--temp-file-name — specify file name of the generated Elm');
-    console.log('');
+
+    helpText.forEach(function(helpLine) {
+        console.log(helpLine);
+    });
+
 } else {
 
     const readFile = require('fs-readfile-promise');
@@ -60,15 +27,15 @@ if (!argv.from) {
     const showTime = argv['show-time'];
 
     const startTime = new Date().getTime();
-    var afterReadTime;
+    let afterReadTime;
 
-    var valuesBelow = argv.hasOwnProperty('values-below') || false;
-    var withValues = argv.hasOwnProperty('with-values') || false;
-    var onlyValues = argv.hasOwnProperty('only-values') || false;
+    const valuesBelow = argv.hasOwnProperty('values-below') || false;
+    const withValues = argv.hasOwnProperty('with-values') || false;
+    const onlyValues = argv.hasOwnProperty('only-values') || false;
 
     readFile(argv.from)
            .then(function(buffer) {
-               var lines = buffer.toString().split('\n');
+               const lines = buffer.toString().split('\n');
                const imports = (lines[0].indexOf(';') === 0) ? lines.shift().split(';').slice(1) : [];
                const expressions = lines.slice(0,-1);
                return {
